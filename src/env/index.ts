1634245f488e6f890d2e5c444a1e017bbf2b74c5
Vinicius Cestari <viniciuscestari01@gmail.com>
2023-08-30 16:18:59 -0300

    creating the base of the application

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string(),
+  DATABASE_URL: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables') // stop the application
+}
+
+export const env = _env.data
